648e1ab5564515e158f380a028933d9eb8eccc6a
BVezilic <banex93@gmail.com>
2017-07-17 19:29:27 +0200

    added function calculate_treshlod in checking.py fixed histogram plotting in histogram_calc.py

--- histogram_calc.py
+++ histogram_calc.py
@@ -2,33 +2,33 @@ import cv2
 from matplotlib import pyplot as plt
 
 
-
-def test(image_path):
+def test(image_path, mask_path):
     img = cv2.imread(image_path)  # Read image on image_path
-    mask = cv2.imread("C:/Users/NT1/Desktop/ssip/SSIP_17/e_optha_EX/Annotation_EX/E0006265/DS000F4K_EX.png", 0)  # Read mask on mask_path
-    img = cv2.GaussianBlur(img, (5, 5), 0)  # Apply Gaussian blur
+    mask = cv2.imread(mask_path, 0)  # Read mask on mask_path
+    img = cv2.GaussianBlur(img, (3, 3), 0)  # Apply Gaussian blur
     img = img[:, :, 1]  # Green channel BGR
-    img = 255 - img  # Invert green channel
-    #img = cv2.resize(img, (925, 614))  # Resize for easier representation
-    #mask = cv2.resize(mask, (925, 614))
+    # img = 255 - img  # Invert green channel
+    # img = cv2.resize(img, (925, 614))  # Resize for easier representation
+    # mask = cv2.resize(mask, (925, 614))
 
-    result = img * mask #apply mask on image
+    result = img * mask  #apply mask on image
 
     hist = cv2.calcHist([result], [0], None, [256], [0, 256]) #calculate histogram
 
+    plt.hist(result.ravel(), 256, [1, 256])
+    plt.show()
     # Show image
     #cv2.imshow('mask', mask)
-    #cv2.imshow('img', img)
-    print(hist)
-    #plt.imshow(hist, interpolation='nearest') #show histogram
-    #plt.show()
-    #cv2.imshow('histogram', hist)
-    cv2.waitKey()
-    cv2.destroyAllWindows()
+    # cv2.imshow('img', img)
+    #
+    # cv2.imshow('result', result)
+    # cv2.waitKey()
+    # cv2.destroyAllWindows()
 
 if __name__ == '__main__':
-    test(image_path="C:/Users/NT1/Desktop/ssip/SSIP_17/e_optha_EX/EX/E0006265/DS000F4K.jpg")
-         #mask_path="C:/Users/NT1/Desktop/ssip/SSIP_17/e_optha_MA/Annotation_MA/E0002477/C0022963.png" )
+    test(image_path="d:/Projects/SSIP/SSIP_17/e_optha_EX/EX/E0019457/C0001273.jpg",
+         mask_path="d:/Projects/SSIP/SSIP_17/e_optha_EX/Annotation_EX/E0019457/C0001273_EX.png")
+
 
 
 
